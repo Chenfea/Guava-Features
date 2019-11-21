8098b394e898ecaa1c9fad59c2aca77e3f9913c8
Feng Chen <Feng_Chen@epam.com>
2019-11-21 15:09:36 +0800

    event class modification

--- src/main/java/eventbus/Event.java
+++ src/main/java/eventbus/Event.java
@@ -2,7 +2,8 @@ package eventbus;
 
 import com.google.common.eventbus.EventBus;
 import com.google.common.eventbus.Subscribe;
-import org.junit.jupiter.api.Test;
+import org.junit.Test;
+//import org.junit.jupiter.api.Test;
 
 /****
  *   Created by Feng Chen on 9/14/2019
@@ -18,7 +19,7 @@ public class Event {
     public void testEventBus() {
         EventBus eventBus = new EventBus();
         eventBus.register(new Event());
-        eventBus.post("hello, this is new message!");
+        eventBus.post("hello, this is new message!yeah");
     }
 
 }
